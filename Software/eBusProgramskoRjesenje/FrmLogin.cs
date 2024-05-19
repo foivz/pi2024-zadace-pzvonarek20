e6195b328a04f89d3467957fa1de5df30eb9ab68
DESKTOP-MOV76V4\HP <pzvonarek20@student.foi.hr>
2024-05-19 11:59:36 +0200

    Implementacija provjere prijave korisnika Dodana forma FrmLogin koja omogućuje prijavu korisnika (privremeno), kasnije omogućiti prijavu preko baze

---
+++ Software/eBusProgramskoRjesenje/FrmLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eBusProgramskoRjesenje
+{
+    public partial class FrmLogin : Form
+    {
+        string username = "korisnik";
+        string password = "password";
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Lozinka nije unesena!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else 
+            {
+                if (txtUsername.Text == username && txtPassword.Text == password)
+                {
+                    MessageBox.Show("Dobrodošli!", "Uspiješno ste prijavljeni", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else 
+                {
+                    MessageBox.Show("Krivi unos podataka!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+    }
+}
