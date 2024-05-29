91047fb82b09528ed9396e40dd07bfe10a8a9cf4
DESKTOP-MOV76V4\HP <pzvonarek20@student.foi.hr>
2024-05-29 23:17:00 +0200

    Poboljšano korisničko iskustvo. Promjenjen je prikaz naziv stupaca unutar DataGridViewa, pomoću DisplayName unutar klase vozilo. Zvonarek Patrik - 29.05.2024. 23:16

--- Software/eBusProgramskoRjesenje/Models/Vozilo.cs
+++ Software/eBusProgramskoRjesenje/Models/Vozilo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,22 @@ namespace eBusProgramskoRjesenje.Models
 {
     public class Vozilo
     {
+        [DisplayName("ID vozila")]
         public int IdVozila { get; set; }
+
+        [DisplayName("Model vozila")]
         public string ModelVozila { get; set; }
+
+        [DisplayName("Vrsta vozila")]
         public string IdVrsteVozila { get; set;}
+
+        [DisplayName("Tablica vozila")]
         public string TablicaVozila { get; set; }
+
+        [DisplayName("Namjena vozila")]
         public string NamjenaVozila { get; set; }
+
+        [DisplayName("Detaljne informacije")]
         public string DetaljneInformacije { get; set; }
     }
 }
